9c24b841504c694f3693fd0a106ff7f08b988078
Andrii Nykolenko <nikolandre14@gmail.com>
2023-02-15 19:16:31 +0200

    remode insecret token

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 
 import telebot
 
-import config
 from parser import WordParser
 
-TOKEN = os.environ.get("BOT_TOKEN") or config.BOT_TOKEN
+TOKEN = os.environ.get("BOT_TOKEN")
 
 
 bot = telebot.TeleBot(TOKEN)
